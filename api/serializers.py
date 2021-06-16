c76d17299222f610e2f3e8121c5d26218b9ee8f5
jhashi19 <j.hashi19@gmail.com>
2021-06-16 22:00:29 +0900

    ログイン作成

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Task
 
@@ -17,13 +16,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = get_user_model().objects.create_user(**validated_data)
-        Token.objects.create(user=user)
-        return user
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'id': {'read_only': True}
+        }
 
 
 class TaskSerializer(serializers.ModelSerializer):
--- api/views.py
+++ api/views.py
@@ -11,7 +11,7 @@ from .serializers import (
 
 class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny]
 
 
 class ObtainTokenPairWithColorView(TokenObtainPairView):
